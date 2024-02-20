dd6c988b2db4cdc8e883388d3e8f6dcdcd92ad6c
Marvel9012 <marvelkingdom.9012@gmail.com>
2024-02-20 08:26:16 +0530

    Add project files.

---
+++ Demo_Code/Demo_Code.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+using namespace std;
+class Sample
+{
+	int* nptr;
+public:
+	Sample(int x)
+	{
+		nptr = new int(x);
+	}
+	void print()
+	{
+		cout << *nptr << endl;
+	}
+};
+int main()
+{
+	Sample s1(10);
+	s1.print();
+}
